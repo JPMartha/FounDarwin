f36b1e56241a4524810c769b28148d63bf44dd97
JPMartha <jpmartha.jp@gmail.com>
2016-05-18 04:05:27 +0900

    Fix run-tests.sh

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,7 +6,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	export SWIFT_VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-05-09-a
 	wget https://swift.org/builds/development/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
 	tar xzf $SWIFT_VERSION-ubuntu14.04.tar.gz
-	export PATH="${PWD}"/${SWIFT_VERSION}-ubuntu14.04/usr/bin:"${PATH}"
+	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin:${PATH}"
 	cd "$DIR"
 else
 	export SWIFT_VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-05-03-a
