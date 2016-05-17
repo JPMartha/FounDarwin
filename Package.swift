121679496710130afca5737d1807904ebc3d873e
JPMartha <jpmartha.jp@gmail.com>
2016-05-18 03:48:08 +0900

    Fix .travis.yml

--- Package.swift
+++ Package.swift
@@ -17,5 +17,6 @@ let package = Package(
             name: "PackageBuild",
             dependencies: ["Directory"]
         )
-    ]
+    ],
+    exclude: ["scripts"]
 )
--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -ev
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	DIR="$(pwd)"
