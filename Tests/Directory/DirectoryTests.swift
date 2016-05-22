2ca2819c432e41552d4d3149c7df2c2a243adb40
JPMartha <jpmartha.jp@gmail.com>
2016-05-22 13:29:07 +0900

    Improve DirectoryTests.swift

--- Tests/Directory/DirectoryTests.swift
+++ Tests/Directory/DirectoryTests.swift
@@ -3,8 +3,6 @@ import XCTest
 
 final class DirectoryTests: XCTestCase {
     func testCurrentDirectory() {
-        #if os(Linux)
-        #else
         var cd = String()
         do {
             cd = try currentDirectoryPath()
@@ -19,184 +17,147 @@ final class DirectoryTests: XCTestCase {
         }
         XCTAssertNotNil(path)
         print(path)
-        #endif
     }
     
-    func currentDirectoryWithDarwin() -> String? {
+    func currentDirectoryForTest() -> String? {
         #if os(Linux)
+            let cwd = getcwd(nil, 0)
+            guard let cd = cwd else {
+                free(cwd)
+                XCTFail("Cannot getcwd.")
+                return nil
+            }
         #else
             let cwd = getcwd(nil, Int(PATH_MAX))
             guard let cd = cwd else {
                 free(cwd)
-                XCTFail()
+                XCTFail("Cannot getcwd.")
                 return nil
             }
-            guard let path = String(validatingUTF8: cd) else {
-                XCTFail()
-                return nil
-        }
-        
-        return path
         #endif
+        
+        guard let path = String(validatingUTF8: cd) else {
+            XCTFail("Cannot validatingUTF8.")
+            return nil
+        }
+        return path
     }
     
     func testChangeDirectory() {
         #if os(Linux)
+            
+            // TODO:
+            
         #else
-            guard let path1 = currentDirectoryWithDarwin() else {
+            guard let path1 = currentDirectoryForTest() else {
                 XCTFail()
                 return
             }
-        print(path1)
-        
-        do {
-            try changeDirectory(path: "\(path1)/.build")
-        } catch {
-            XCTAssertThrowsError(DirectoryError.CannotChangeDirectory)
-            return
-        }
-        
-        // TODO: Verify
-        
-        guard let path2 = currentDirectoryWithDarwin() else {
-            XCTFail()
-            return
-        }
-        print(path2)
-        
-        // It is necessary for making a success of the other tests
-        //   to change working directory into path1.
-        
-        do {
-            try changeDirectory(path: path1)
-        } catch {
-            XCTAssertThrowsError(DirectoryError.CannotChangeDirectory)
-            return
-        }
+            print(path1)
+            
+            do {
+                try changeDirectory(path: "\(path1)/.build")
+            } catch {
+                XCTAssertThrowsError(DirectoryError.CannotChangeDirectory)
+                return
+            }
+            
+            // TODO: Verify
+            
+            guard let path2 = currentDirectoryForTest() else {
+                XCTFail()
+                return
+            }
+            print(path2)
+            
+            // It is necessary for making a success of the other tests
+            //   to change working directory into path1.
+            
+            do {
+                try changeDirectory(path: path1)
+            } catch {
+                XCTAssertThrowsError(DirectoryError.CannotChangeDirectory)
+                return
+            }
         #endif
     }
     
     func testCreateDirectory() {
         #if os(Linux)
+
+            // TODO:
+            
         #else
-        guard let path = currentDirectoryWithDarwin() else {
-            XCTFail()
-            return
-        }
-        print(path)
-        
-        do {
-            try createDirectory(path: "\(path)/testCreateDirectory")
-        } catch {
-            XCTAssertThrowsError(DirectoryError.CannotCreateDirectory)
-            return
-        }
-        
-        XCTAssertEqual(access("\(path)/testCreateDirectory", F_OK), 0)
-        
-        rmdir("\(path)/testCreateDirectory")
+            guard let path = currentDirectoryForTest() else {
+                XCTFail()
+                return
+            }
+            print(path)
+            
+            do {
+                try createDirectory(path: "\(path)/testCreateDirectory")
+            } catch {
+                XCTAssertThrowsError(DirectoryError.CannotCreateDirectory)
+                return
+            }
+            
+            XCTAssertEqual(access("\(path)/testCreateDirectory", F_OK), 0)
+            
+            rmdir("\(path)/testCreateDirectory")
         #endif
     }
     
     func testIsAccessibleDirectory() {
         #if os(Linux)
+
+            // TODO:
+
         #else
-            guard let path = currentDirectoryWithDarwin() else {
+            guard let path = currentDirectoryForTest() else {
                 XCTFail()
                 return
             }
-        print(path)
-        
-        let testAccessibleDirectory = "\(path)/testAccessibleDirectory"
-        XCTAssertFalse(isAccessibleDirectory(path: testAccessibleDirectory))
-        
-        guard mkdir(testAccessibleDirectory, S_IRWXU | S_IRWXG | S_IRWXO) == 0 || errno == EEXIST else {
-            XCTFail("Cannot mkdir")
-            return
-        }
-        XCTAssertTrue(isAccessibleDirectory(path: testAccessibleDirectory))
-        
-        rmdir(testAccessibleDirectory)
-        XCTAssertFalse(isAccessibleDirectory(path: testAccessibleDirectory))
+            print(path)
+            
+            let testAccessibleDirectory = "\(path)/testAccessibleDirectory"
+            XCTAssertFalse(isAccessibleDirectory(path: testAccessibleDirectory))
+            
+            guard mkdir(testAccessibleDirectory, S_IRWXU | S_IRWXG | S_IRWXO) == 0 || errno == EEXIST else {
+                XCTFail("Cannot mkdir")
+                return
+            }
+            XCTAssertTrue(isAccessibleDirectory(path: testAccessibleDirectory))
+            
+            rmdir(testAccessibleDirectory)
+            XCTAssertFalse(isAccessibleDirectory(path: testAccessibleDirectory))
         #endif
     }
     
     func testRemoveDirectory() {
         #if os(Linux)
+
+            // TODO:
+
         #else
-        guard let path = currentDirectoryWithDarwin() else {
-            XCTFail()
-            return
-        }
-        print(path)
-        
-        let testRemoveDirectory = "\(path)/testRemoveDirectory"
-        guard mkdir(testRemoveDirectory, S_IRWXU | S_IRWXG | S_IRWXO) == 0 || errno == EEXIST else {
-            XCTFail()
-            return
-        }
-        
-        XCTAssertEqual(access(testRemoveDirectory, F_OK), 0)
-        
-        removeDirectory(path: testRemoveDirectory)
-        
-        XCTAssertNotEqual(access(testRemoveDirectory, F_OK), 0)
-        #endif
-    }
-        /*
-        var currentDirectory = String()
-        let testDirectoryName = "testChangeDirectory"
-        do {
-            currentDirectory = try currentDirectoryPath()
-        } catch {
-            XCTAssertThrowsError(DirectoryError.CannotGetCurrentDirectory)
-            return
-        }
-        guard !currentDirectory.isEmpty else {
-            XCTAssertThrowsError(DirectoryError.CannotGetCurrentDirectory)
-            return
-        }
-        
-        #if os(Linux)
-            print("CurrentDirectoryPath: \(currentDirectory)")
-        #else
-            print("CurrentDirectoryPath: \(currentDirectory)")
-            let fp = popen("echo $PWD", "r")
-            
-            // FIXME: Hard-Coding
-            let bufferSize = 4096
-            var buffer = [Int8](repeating: 0, count: bufferSize + 1)
-            fgets(&buffer, Int32(bufferSize), fp)
-            guard let cd = String(validatingUTF8: buffer) else {
-                XCTFail("Cannot validatingUTF8")
+            guard let path = currentDirectoryForTest() else {
+                XCTFail()
                 return
             }
-            pclose(fp)
+            print(path)
             
-            XCTAssertEqual(String(cd.characters.dropLast()), currentDirectory)
+            let testRemoveDirectory = "\(path)/testRemoveDirectory"
+            guard mkdir(testRemoveDirectory, S_IRWXU | S_IRWXG | S_IRWXO) == 0 || errno == EEXIST else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqual(access(testRemoveDirectory, F_OK), 0)
+            
+            removeDirectory(path: testRemoveDirectory)
+            
+            XCTAssertNotEqual(access(testRemoveDirectory, F_OK), 0)
         #endif
-        
-        do {
-            try createDirectory(path: "\(currentDirectory)/\(testDirectoryName)")
-        } catch {
-            XCTFail()
-            return
-        }
-        #if os(Linux)
-            // TODO:
-        #else
-            XCTAssertTrue(access("\(currentDirectory)/\(testDirectoryName)", F_OK) == 0)
-        #endif
-        
-        do {
-            try changeDirectory(path: "\(currentDirectory)/\(testDirectoryName)")
-        } catch {
-            XCTFail("Cannot changeDirectory")
-            return
-        }
-        rmdir("\(currentDirectory)/\(testDirectoryName)")
- */
-    //}
+    }
 }
 
 extension DirectoryTests {
